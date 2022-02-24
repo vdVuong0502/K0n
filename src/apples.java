a0114cb5745f49e23fc7ac77cc57075a804a9148
vdVuong0502 <bejamin.vnsharing@gmail.com>
2022-02-24 08:43:24 +0700

    avrg 24/2

--- src/apples.java
+++ src/apples.java
@@ -3,18 +3,12 @@ import java.util.Scanner;
 public class apples {
 
 	public static void main(String[] args) {
-		int age = 56;
-		if (age < 50) {
-			System.out.println("You're still young");
-		} else {
-			System.out.println("You're not young anymore");
-			if (age > 75)
-			{
-				System.out.println("You're really old");
-				
-			} else {
-				System.out.println("You're not really that old");
-			}
+		Scanner input = new Scanner(System.in);
+		int total = 0;
+		int grade;
+		int average;
+		int count = 0;
+		
 		}
 	}
 }
