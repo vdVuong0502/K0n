97651435bc0443f3883552cfadfe5fcf606705a9
vdVuong0502 <bejamin.vnsharing@gmail.com>
2022-03-02 08:48:58 +0700

    25 tut

--- src/apples.java
+++ src/apples.java
@@ -3,20 +3,7 @@ import java.util.Scanner;
 public class apples {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int total = 0;
-		int grade;
-		int average;
-		int count = 0;
-		
-		while (count < 10) {
-			grade =  input.nextInt();
-			total += grade;
-			count++;
-		}
-		
-		average = total / (count + 1);
-		System.out.println("average is " + average);
+		System.out.println(Math.min(8.6, 5.2));
 		
 	}
 }
