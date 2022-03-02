a2f115ca4822b58a7a4e820401e57f8a3ebef9c6
vdVuong0502 <bejamin.vnsharing@gmail.com>
2022-03-02 09:14:54 +0700

    rand

---
+++ src/dumbCalculator/RandomNumber.java
@@ -0,0 +1,14 @@
+package dumbCalculator;
+import java.util.Random;
+
+public class RandomNumber {
+	public static void main(String[] args) {
+		Random dice = new Random();
+		int number;
+		for (int counter = 1; counter <= 10; counter++) {
+			number = dice.nextInt(6);
+			System.out.println(number + " ");
+		}
+	}
+	
+}
