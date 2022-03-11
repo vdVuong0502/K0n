27d66e29af77b535711dd1a0cac7d5f04ec850e6
vdVuong0502 <bejamin.vnsharing@gmail.com>
2022-03-11 09:13:46 +0700

    tut 30

--- src/apples.java
+++ src/apples.java
@@ -1,9 +1,22 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class apples {
 
 	public static void main(String[] args) {
 		System.out.println(Math.min(8.6, 5.2));
 		
+		Random rand = new Random();
+		int[] dices = new int[7];
+		
+		for (int roll = 1; roll < 1000; roll++) {
+			++dices[1+rand.nextInt(6)];
+		}
+		
+		System.out.println("Dice face \t Freq");
+		
+		for (int face = 1; face < dices.length; face++) {
+			System.out.println(face + "\t\t\s" + dices[face]);
+		}
 	}
 }
+  
--- src/dumbCalculator/RandomNumber.java
+++ src/dumbCalculator/RandomNumber.java
@@ -3,12 +3,11 @@ import java.util.Random;
 
 public class RandomNumber {
 	public static void main(String[] args) {
-		Random dice = new Random();
-		int number;
-		for (int counter = 1; counter <= 10; counter++) {
-			number = dice.nextInt(6);
-			System.out.println(number + " ");
+		Random rand = new Random();
+		for (int i = 0; i < 20; i++) {
+			System.out.println(rand.nextInt(6));
 		}
+		
 	}
 	
 }
