b6e0f96ec7a7f2462942443324d1f5d53015f8e6
vdVuong0502 <bejamin.vnsharing@gmail.com>
2022-03-11 09:27:16 +0700

    tut 32

--- src/apples.java
+++ src/apples.java
@@ -3,12 +3,16 @@ public class apples {
 	public static void main(String[] args) {
 		int k0n[] = {3, 4, 5, 6, 7};
 		
-		int total = 0;
+		change(k0n);
 		for (int x : k0n) {
-			total += x;
+			System.out.println(x);
 		}
-		
-		System.out.println(total);
 	}
+	
+	public static void change(int  x[]) {
+		for(int counter = 0; counter < x.length; counter++) {
+			x[counter] += 5; 
+		}
+	};
 }
   
